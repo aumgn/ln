01bacddd4fe44128e99a8e5cf66d13a531cd1e8f
aumgn <aumgn@free.fr>
2012-09-06 22:44:27 +0200

    Derp

--- db/shortened_link.rb
+++ db/shortened_link.rb
@@ -12,7 +12,7 @@ class ShortenedLink
   property :name,             String, required: true, unique: true, length: 16
   property :url,              String, required: true, format: :url
   property :created_at,       DateTime
-  property :update_at,        DateTime
+  property :updated_at,        DateTime
   belongs_to :user
 
   validates_format_of :name, with: /[0-9a-zA-Z]{1,16}/,
